68f41d9871a2eba38e9c3b6eb2f97783a468a8e8
stanislav <stasfed16@gmail.com>
2025-02-13 14:56:00 +0500

    fix

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name="Наименование")
     description = models.TextField(verbose_name="Описание", blank=True, null=True)
@@ -14,19 +27,6 @@ class Product(models.Model):
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
         ordering = ["category", "name"]
-
-    def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Наименование")
-    description = models.TextField(verbose_name="Описание", blank=True, null=True)
-
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
-        ordering = ["name"]
 
     def __str__(self):
         return self.name
